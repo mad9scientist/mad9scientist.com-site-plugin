578030da6f49fd00d885432bbcf1328da33b2326
Chris Holbrook <chris@madscitech.com>
2015-02-07 02:56:18 -0600

    Updated Backup JQuery Version to 2.1.3

--- m9s-site.php
+++ m9s-site.php
@@ -33,7 +33,7 @@ function jQuery_inject(){
 
 function jQuery_bkp(){
 ?>
-<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.0.min.js"><\/script>')</script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-2.1.3.min.js"><\/script>')</script>
 <?php
 }
 
